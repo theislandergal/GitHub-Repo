656fd36aa9c2082ea6a7bbccf84053b881a2fb2b
theislandergal <122315818+theislandergal@users.noreply.github.com>
2023-01-12 15:53:56 -0500

    Pong started, visuals and images added

--- BackIntoCode/js/script.js
+++ BackIntoCode/js/script.js
@@ -7,7 +7,10 @@ author, and this description to match your project!
 */
 
 "use strict";
+fill(21,81,36); // Green team
+fill(150,65,19); // Orange team
 
+var verticalOffset
 
 /**
 Description of preload
@@ -21,8 +24,15 @@ function preload() {
 Description of setup
 */
 function setup() {
- createCanvas(500,500);
- background(0,255,0);
+ createCanvas(700,500);
+ background(187,196,204);
+ stroke(255,0,0);
+ strokeWeight(3);
+ circle(350,250,80);
+ fill(237,240,240);
+ strokeWeight(5);
+ stroke(80,149,226);
+ line(350,0,350,500);
 }
 
 
@@ -30,5 +40,17 @@ function setup() {
 Description of draw()
 */
 function draw() {
-
+    
 }
+
+function keyPressed(){
+    if (keyCode == UP_ARROW && verticalOffset > 0){
+       verticalOffset = verticalOffset - 10;
+     } else if (keyCode == DOWN_ARROW && verticalOffset <= 1000){
+       verticalOffset = verticalOffset + 10;
+     } else if (keyCode == W && verticalOffset > 0){
+        verticalOffset = verticalOffset - 10;
+      } else if (keyCode == S && verticalOffset <= 1000){
+        verticalOffset = verticalOffset + 10;
+      }
+    }
