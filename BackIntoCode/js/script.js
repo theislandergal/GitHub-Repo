68fae10a3046d2a65b78f9e7ba452f0e4781074c
theislandergal <122315818+theislandergal@users.noreply.github.com>
2023-01-19 12:07:39 -0500

    Commented

--- BackIntoCode/js/script.js
+++ BackIntoCode/js/script.js
@@ -7,28 +7,30 @@ author, and this description to match your project!
 */
 
 "use strict";
-//Setting 
-let ponyo;
-let xinitPonyo;
-let yinitPonyo;
+//Setting up Ponyo Variable
+let ponyo; // image holder
+let xinitPonyo; // starting x position
+let yinitPonyo; // starting y position
 
-let rad = 40;
-let xspeed = 1.5;
-let yspeed = 1.5;
-let xdirection = 1;
-let ydirection = 1;
+let rad = 40;  // Size of the image 
+let xspeed = 1.5;  // speed of the image on the x axis
+let yspeed = 1.5; // speed of the image on the y axis
+let xdirection = 1; // variable holder for direction change on the x axis
+let ydirection = 1; // variable holder for direction change on the y axis
 
-let verticalOffsetGreen;
-let verticalOffsetOrange;
-let xinitGreen;
-let yinitGreen;
-let xinitOrange;
-let yinitOrange;
+// Setting up team paddle variables
+let verticalOffsetGreen; // variable holder for paddle direction change on the y axis
+let verticalOffsetOrange; // variable holder for paddle direction change on the y axis
+let xinitGreen; // starting x position green team
+let yinitGreen; // starting y position green team
+let xinitOrange; // starting x position orange team
+let yinitOrange; // starting y position orange team
 
 /**
 Description of preload
 */
 function preload() {
+// Preloading Ponyo image from assets
 ponyo = loadImage('assets/images/ponyo.png');
 }
 
@@ -36,16 +38,17 @@ ponyo = loadImage('assets/images/ponyo.png');
 Description of setup
 */
 function setup() {
- createCanvas(700,500);
- frameRate(30);
- ellipseMode(RADIUS);
- // Set the starting position of the shape
+ createCanvas(700,500); // canvas size
+ frameRate(30); // framerate for 
+ ellipseMode(RADIUS); // ellipse mode based on Radius size
+ // Set the starting position of Ponyo and paddles
   xinitPonyo = width / 2;
   yinitPonyo = height / 2;
   xinitGreen = width - 30;
   yinitGreen = height / 2 - 35;
   xinitOrange = 30;
   yinitOrange = height / 2 - 35;
+  // Setting first variable for vertical off sets
   verticalOffsetGreen = yinitGreen;
   verticalOffsetOrange = yinitOrange;
 }
@@ -55,7 +58,8 @@ function setup() {
 Description of draw()
 */
 function draw() {
-  background(187,196,204);
+  background(187,196,204); // constantly drawing background in draw
+  // Setting visual elements, Center circle and line.
   stroke(255,0,0);
   strokeWeight(3);
   fill(237,240,240);
@@ -65,11 +69,14 @@ function draw() {
   line(350,0,350,500);
   noStroke();
   fill(0,0,255);
+  // Loading image and paddles with inital positions
   image(ponyo,xinitPonyo,yinitPonyo,rad,rad);
+  // Calling paddle moving functions
   movePaddleGreen(xinitGreen,verticalOffsetGreen);
   movePaddleOrange(xinitOrange,verticalOffsetOrange);
 
-    // Update the position of the shape
+    // Update the position of Ponyo
+    // Code taken from the p5js reference
     xinitPonyo = xinitPonyo + xspeed * xdirection;
     yinitPonyo = yinitPonyo + yspeed * ydirection;
   
@@ -83,6 +90,7 @@ function draw() {
     }
 }
 
+// Paddle creation functions
 function movePaddleGreen(xinitGreen,yinitGreen){
   fill(21,81,36); // Green team
   rect(xinitGreen,yinitGreen,10,70);
@@ -93,6 +101,7 @@ function movePaddleOrange(xinitOrange,yinitOrange){
   rect(xinitOrange,yinitOrange,10,70);
 }
 
+// Paddle Moving function by key pressing
 function keyPressed(){
     if (keyCode == UP_ARROW && verticalOffsetGreen > 0){
        verticalOffsetGreen = verticalOffsetGreen - 20;
